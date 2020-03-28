24e45b23f5c11ac9a47b531ed46290bbb80586e5
mirkoperillo <mirko.perillo@gmail.com>
2020-03-28 18:37:04 +0100

    fix

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const Telegraf = require('telegraf')
 
 let bot = new Telegraf(process.env.BOT_TOKEN);
 const port = process.env.PORT || 3000;
+const HEROKU_URL = process.env.HEROKU_URL;
+const BOT_TOKEN = process.env.BOT_TOKEN;
+
+bot.telegram.setWebhook(`${HEROKU_URL}/bot${BOT_TOKEN}`);
+bot.startWebhook(`/bot${BOT_TOKEN}`, null, port);
 
 bot.start((ctx) => ctx.reply('Welcome!'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 
-bot.telegram.setWebhook(`${HEROKU_URL}/bot${BOT_TOKEN}`);
-bot.startWebhook(`/bot${BOT_TOKEN}`, null, port);
+
 
