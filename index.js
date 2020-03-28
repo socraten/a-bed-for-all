0053cdc2244d415b997e5ac0354523680266fa55
mirkoperillo <mirko.perillo@gmail.com>
2020-03-28 18:23:44 +0100

    fix

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ require('dotenv').config()
 const Telegraf = require('telegraf')
 
 let bot = new Telegraf(process.env.BOT_TOKEN)
-
-if (process.env.NODE_ENV === 'production') {
-    bot.telegram.setWebhook(process.env.HEROKU_URL+'/bot'+bot.token);
-    // bot.setWebHook(process.env.HEROKU_URL+'/bot/' + bot.token);
-    bot.startWebhook('/bot'+bot.token, null, 3000);
-}
+const port = process.env.PORT || 2000;
 
 bot.start((ctx) => ctx.reply('Welcome!'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.launch()
+
+if (process.env.NODE_ENV == 'production') {
+    bot.telegram.setWebhook(`S{HEROKU_URL}/bot${BOT_TOKEN}`);
+    // bot.setWebHook(process.env.HEROKU_URL+'/bot/' + bot.token);
+    bot.startWebhook(`/bot${BOT_TOKEN}`, null, port);
+}else {
+  bot.launch()
+}
 
