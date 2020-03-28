ec25d2e9b5dac2e2436d078f9a9e0ed5a967e231
Mario Alexandro Santini <alexmario74@gmail.com>
2020-03-28 17:23:58 +0100

    fixed for heroku env

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ require('dotenv').config()
 
 const Telegraf = require('telegraf')
 
+let bot = new Telegraf(process.env.BOT_TOKEN)
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+if (process.env.NODE_ENV === 'production') {
+    bot.setWebHook(process.env.HEROKU_URL + bot.token)
+}
+
 bot.start((ctx) => ctx.reply('Welcome!'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
